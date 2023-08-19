69713c0d3f17afb2bc834955dd1a8fc26c9bc97e
Jimena Barranco <jb0125525@gmail.com>
2023-08-18 18:17:08 -0600

    review code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,29 @@
 
 
+/*
+1.Se estan declarando dos variables para obtener la info
+del url de github.
+2.Se seleccionan los elementos del html con sus respectivos id's
+pero en el html faltaba un id.
 
+
+
+ */
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+// para hacer la seleccion en name hace falta un . y a blog
+//cambiar el # por un .
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
 
-
+/* En esta funcion asincrona se va a tomar el nombre de 
+usuario como argumento */
 async function displayUser(username) {
 
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  //hay que convert
   const responseJ = await response.json();
   console.log(responseJ);
   console.log(response);
